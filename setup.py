c106ba17913d5e4ab0706f5489126b51f3b4816e
GREGORY WILLIAM JOSEPH D. LIU <gram.liu226@gmail.com>
2019-10-16 17:00:39 -0400

    Initialized setup files

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='Craft112',
+    version='0.0.1',
+    packages=['pyglet'],
+    url='https://github.com/Lexicographical/CMU-15112',
+    license='GPL 3.0',
+    author='Gregory Liu <Lexicographical>',
+    author_email='gdl2@andrew.cmu.edu',
+    description='Voxel open-world fame'
+)
